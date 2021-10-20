3141ea6a567fd08c5aac653c8668cc167a97710f
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-20 13:32:33 +0200

    boiler plate

---
+++ src/services/users/routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const userRouter = express.Router()
+
+userRouter.route()
+
+
+export default userRouter
---
+++ src/services/volunteers/routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const volunteerRouter = express.Router()
+
+volunteerRouter.route()
+
+
+export default volunteerRouter
