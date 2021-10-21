e522fb70993471089883ebf5af970a89c80ed088
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-21 10:12:49 +0200

    user schema

---
+++ src/db/schema/volunteer/loginSchema.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const {Schema,model} = mongoose
+
+const loginSchema = new Schema({
+    name:{type:String,required:true},
+    surname:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    googleId: { type: String },
+
+},{timestamps:true})
+
+export default model('user',loginSchema)
