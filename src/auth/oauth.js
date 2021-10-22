226d3a11851a59c8067655f711717452d2571c6a
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-23 00:26:11 +0200

    oauth

--- src/auth/oauth.js
+++ src/auth/oauth.js
@@ -1,6 +1,7 @@
 import GoogleStrategy from 'passport-google-oauth20'
 import passport from 'passport'
 import userModel from '../db/schema/user/user.js'
+import { jwtAuth } from './tools.js'
 
 
 const googleStrategy = new GoogleStrategy({
@@ -12,10 +13,26 @@ async(accessToken,refreshToken,profile,passportNext)=>{
     try {
         const user = await userModel.findOne({googleId:profile.id})
         if(user){
-            const tokens = awaut jwt
+            const tokens = await jwtAuth(user)
+            passportNext(null,{tokens})
+        }else{
+            const newUser = {
+                name:profile.name.givenName,
+                surname:profile.name.surname,
+                email:profile.emails[0].value,
+                googleId:profile.id
+            }
+            const createUser = new userModel(newUser)
+            const saveUser = createUser.save()
+            const tokens = await jwtAuth(saveUser)
+            passportNext(null,{user:saveUser,tokens})
         }
     } catch (error) {
-        
-    }
-}
-)
+        passportNext(error)
+    }})
+    passport.serializeUser(function(user,passportNext){
+        passportNext(null,user)
+    })
+    export default googleStrategy
+
+
