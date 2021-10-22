c6b9b96a165c864aa54a23e7ef85f9938ece142b
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-22 11:16:11 +0200

    volunteer routes

--- src/services/volunteers/routes.js
+++ src/services/volunteers/routes.js
@@ -1,11 +1,21 @@
 import express from 'express'
+import { allMethods } from './index.js'
 
 const volunteerRouter = express.Router()
 
 volunteerRouter.route('/login')
-.get()
+.post(allMethods.VolunteerLogin)
+
 volunteerRouter.route('/register')
+.post(allMethods.VolunteerRegister)
+
 volunteerRouter.route('/me')
+.get(allMethods.getVolunteerMe)
+.put(allMethods.updateVolunteerMe)
+.delete(allMethods.deleteVolunteerMe)
+
+volunteerRouter.route('/me/details')
+.post(allMethods.postVolunteerDetail)
 
 
 
