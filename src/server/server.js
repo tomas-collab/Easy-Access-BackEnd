b0b4e2431b367f00e1d98993f89d42f3f08bd3aa
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-22 11:20:24 +0200

    server fix and user routes

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import userRouter from './services/users/routes.js'
-import volunteerRouter from './services/volunteers/routes.js'
+import userRouter from '../services/users/routes.js'
+import volunteerRouter from '../services/volunteers/routes.js'
 import listEndpoints from 'express-list-endpoints'
 const server = express()
 
@@ -18,7 +18,7 @@ server.use('/volunteers',volunteerRouter)
 
 mongoose.connect(process.env.MONGO_URL)
 mongoose.connection.on('connected',()=>{
-    console.log('mongoose connected')
+    console.log('mongoose connected successfully')
     server.listen(port,async()=>{
         console.table(listEndpoints(server))
         console.log('server listening on port:',port)
--- src/services/users/routes.js
+++ src/services/users/routes.js
@@ -1,8 +1,22 @@
 import express from 'express'
+import { allMethods } from './index.js'
 
 const userRouter = express.Router()
 
-userRouter.route('/')
+userRouter.route('/login')
+.post(allMethods.UserLogin)
+
+userRouter.route('/register')
+.post(allMethods.UserRegister)
+
+userRouter.route('/me')
+.get(allMethods.getUserMe)
+.put(allMethods.updateUserMe)
+.delete(allMethods.deleteUserMe)
+
+userRouter.route('/me/details')
+.post(allMethods.postUserDetail)
+
 
 
 export default userRouter
