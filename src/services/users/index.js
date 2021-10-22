61178983330423bd0e0075a65224a9619a27b177
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-23 00:38:11 +0200

    user route

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,4 +1,5 @@
 import userModel from '../../db/schema/user/user.js'
+import { JWTAuthMiddleware } from '../../auth/token.js'
 
 const getUsers = async(req,res,next)=>{
     try {
@@ -10,28 +11,32 @@ const getUsers = async(req,res,next)=>{
 }
 const postUser = async(req,res,next)=>{
     try {
-        
+        const newUser = new userModel(req.body)
+        const user = await newUser.save()
+        res.status(200).send(user)
     } catch (error) {
         next(error)
     }
 }
-const getUserMe= async(req,res,next)=>{
+const getUserMe= async(JWTAuthMiddleware,req,res,next)=>{
     try {
-        
+        res.status(200).send(req.user)
     } catch (error) {
         next(error)
     }
 }
-const updateUserMe = async(req,res,next)=>{
+const updateUserMe = async(JWTAuthMiddleware,req,res,next)=>{
     try {
-        
+        const updateUser = await userModel.findByIdAndUpdate(req.user._id,req.body,{new:true})
+        res.status(204).send(updateUser)
     } catch (error) {
         next(error)
     }
 }
 const deleteUserMe = async(req,res,next)=>{
     try {
-        
+        const deleteUser = await userModel.findByIdAndDelete(req.user._id)
+        res.status(202).send('deleted')
     } catch (error) {
         next(error)
     }
