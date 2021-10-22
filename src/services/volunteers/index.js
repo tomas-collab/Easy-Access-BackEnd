fed1f942f6c71fbf0667e648eb4dfa5292e299f9
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-22 11:13:07 +0200

    volunteer routes

--- src/services/volunteers/index.js
+++ src/services/volunteers/index.js
@@ -1,1 +1,70 @@
-import volunteerModel from '../../db/schema/volunteer/volunteer'
+import volunteerModel from '../../db/schema/volunteer/volunteer.js'
+
+const getVolunteers = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const postVolunteer = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const getVolunteerMe= async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const updateVolunteerMe = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const deleteVolunteerMe = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const postVolunteerDetail = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const VolunteerLogin = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+const VolunteerRegister = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const allMethods = {
+    getVolunteers:getVolunteers,
+    postVolunteer:postVolunteer,
+    getVolunteerMe:getVolunteerMe,
+    updateVolunteerMe:updateVolunteerMe,
+    deleteVolunteerMe:deleteVolunteerMe,
+    postVolunteerDetail:postVolunteerDetail,
+    VolunteerLogin:VolunteerLogin,
+    VolunteerRegister:VolunteerRegister
+}
