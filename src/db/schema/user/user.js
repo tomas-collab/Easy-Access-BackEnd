f2ab7285a90fb132eeb9c5896e2e38530633de82
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-22 17:21:49 +0200

    bcrypt

--- src/db/schema/user/user.js
+++ src/db/schema/user/user.js
@@ -1,16 +1,45 @@
 import mongoose from 'mongoose'
-
+import bcrypt from 'bcrypt'
 const {Schema,model} = mongoose
 
-const userLoginSchema = new Schema({
+const userSchema = new Schema({
     name:{type:String,required:true},
     surname:{type:String,required:true},
     email:{type:String,required:true},
-    password:{type:String,required:true},
+    password:{type:String},
     phoneNumber:{type:String},
     googleId: { type: String },
     location:{type:Object}
 
 },{timestamps:true})
 
-export default model('user',userLoginSchema)
+
+  userSchema.pre('save',async function(next){
+      const user = this
+      const plainPw = user.password
+      if(user.isModified('password')){
+          user.password = await bcrypt.hash(plainPw,12)
+      }
+      next()
+  })
+
+  userSchema.methods.toJSON = function(){
+      const userData = this
+      const userObject = userData.toObject()
+      delete userObject.password
+      return userObject
+  }
+
+  userSchema.statics.checkCredentials = async function(email,plainPw){
+      const user = await this.findOne({email})
+      if(user){
+          const isMatch = await bcrypt.compare(plainPw,user.password)
+          if(isMatch) return user
+          else return null
+      }else{
+          return null
+      }
+  }
+
+
+export default model('user',userSchema)
