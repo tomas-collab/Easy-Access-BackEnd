012c7484474eed2d3d337786ef087ce8929c4860
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-23 02:04:12 +0200

    volunteer google strategy

--- src/auth/oauth.js
+++ src/auth/oauth.js
@@ -1,10 +1,11 @@
 import GoogleStrategy from 'passport-google-oauth20'
 import passport from 'passport'
 import userModel from '../db/schema/user/user.js'
+import volunteerModel from '../db/schema/volunteer/volunteer.js'
 import { jwtAuth } from './tools.js'
 
 
-const googleStrategy = new GoogleStrategy({
+export const userGoogleStrategy = new GoogleStrategy({
     clientId:process.env.GOOGLE_OAUTH_ID,
     clientSecret:process.env.GOOGLE_OAUTH_SECRET,
     callbackUrl:`${process.env.API_URL}:${process.env.PORT}/users/googleRedirect`,
@@ -30,9 +31,36 @@ async(accessToken,refreshToken,profile,passportNext)=>{
     } catch (error) {
         passportNext(error)
     }})
+
+export const volunteerGoogleStrategy = new GoogleStrategy({
+    clientId:process.env.GOOGLE_OAUTH_ID,
+    clientSecret:process.env.GOOGLE_OAUTH_SECRET,
+    callbackUrl:`${process.env.API_URL}:${process.env.PORT}/volunteers/googleRedirect`,
+},
+async(accessToken,refreshToken,profile,passportNext)=>{
+    try {
+        const user = await volunteerModel.findOne({googleId:profile.id})
+        if(user){
+            const tokens = await jwtAuth(user)
+            passportNext(null,{tokens})
+        }else{
+            const newVolunteer = {
+                name:profile.name.givenName,
+                surname:profile.name.surname,
+                email:profile.emails[0].value,
+                googleId:profile.id
+            }
+            const createVolunteer = new volunteerModel(newVolunteer)
+            const saveVolunteer = createVolunteer.save()
+            const tokens = await jwtAuth(saveVolunteer)
+            passportNext(null,{volunteer:saveVolunteer,tokens})
+        }
+    } catch (error) {
+        passportNext(error)
+    }})
     passport.serializeUser(function(user,passportNext){
         passportNext(null,user)
     })
-    export default googleStrategy
+
 
 
